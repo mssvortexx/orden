e8eb06aea1c5a30ffd8ee8b928e47afac3ceb3b8
Светка <mssvortex@icloud.com>
2026-09-20 21:53:23 +0300

    .

--- sun_1/task1.cpp
+++ sun_1/task1.cpp
@@ -8,44 +8,45 @@ int main()
     cin >> time;
 
     int length;
-    cin >> length;
-    if (length % 4 != 0)
-    {    
-        cout << "NO" << endl;
-        return 1;
-    }
+    while (cin >> length)
+    {   
+        if (length % 4 != 0)
+        {    
+            cout << "NO" << endl;
+            continue;
+        }
 
-    int half = length / 2;
-    int even[half], odd[half];
-
-
-    int sum = 0;
-    int num = 2;
-    for(int i = 0; i < half; i++)
-    {    
-        even[i] = num;
-        odd[i] = num - 1;
-        sum += num;
-        num +=2;
-    }
-
-    int last = sum;
-    for (int i = 0; i < half - 1; i++)
-    {    
-        last -= odd[i];
-    }
+        int half = length / 2;
+        int even[half], odd[half];
 
 
-    for (int i = 0; i < half; i++)
-    {    
-        cout << even[i] << " ";
-    }
+        int sum = 0;
+        int num = 2;
+        for(int i = 0; i < half; i++)
+        {    
+            even[i] = num;
+            odd[i] = num - 1;
+            sum += num;
+            num +=2;
+        }
 
-    for(int i = 0; i < half - 1; i++)
-    {    
-        cout << odd[i] << " ";
-    }
-    cout << last << endl;
+        int last = sum;
+        for (int i = 0; i < half - 1; i++)
+        {    
+            last -= odd[i];
+        }
 
-    return 0;
+
+        for (int i = 0; i < half; i++)
+        {    
+            cout << even[i] << " ";
+        }
+
+        for(int i = 0; i < half - 1; i++)
+        {    
+            cout << odd[i] << " ";
+        }
+        cout << last << endl;
+
+        return 0;}
 }
