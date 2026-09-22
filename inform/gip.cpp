aa645d13a83819a3e2ab8e2fe9088db3f55ef386
Светка <mssvortex@icloud.com>
2026-09-22 18:43:39 +0300

    .

---
+++ inform/gip.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main ()
+{    
+    double a, b;
+
+    cin >> a;
+    cin >> b;
+
+    cout << sqrt((a * a) + (b * b)) << endl;
+}
