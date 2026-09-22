af914cec41e00e8691dbad45f413c8b9d89b1a69
Светка <mssvortex@icloud.com>
2026-09-22 18:51:36 +0300

    .

---
+++ inform/numb.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{    
+    int n;
+
+    cout << "The next number for the number " << n << " is " << n + 1 << "." << endl;
+    cout << "The previos number for the number " << n << " is " << n - 1 << "." << endl;
+}
