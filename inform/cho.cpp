c15917e11223337bc3e305f754929601cd8d79a7
Светка <mssvortex@icloud.com>
2026-09-22 19:53:42 +0300

    k

--- inform/cho.cpp
+++ inform/cho.cpp
@@ -5,13 +5,10 @@ using namespace std;
 int main()
 {    
     int n;
-    int sum = 0;
 
     cin >> n;
 
-    for (int i = 0; i < 3; i++)
-    {    
-        sum += n % 10;
-    }
-    cout << sum << endl;
+    n += 2 - (n % 2);
+
+    cout << n << endl;
 }
