84c339a914e40a0c185241e82a8a040e7a1e48e5
Светка <mssvortex@icloud.com>
2026-09-23 11:25:24 +0300

    nn

--- inform/snail.cpp
+++ inform/snail.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main()
 {    
-    int h, a, b, x;
+    int h, a, b, x; 
 
     cin >> h;
     cin >> a;
     cin >> b;
 
-    x = (h - b) / (a - b);
+    x = (((h - b) + (a - b - 1))/ (a - b));
 
     cout << x << endl;
 }
