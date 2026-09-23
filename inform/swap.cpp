3eb875b7db2b809c4942f9f9ff158a280336be9a
Светка <mssvortex@icloud.com>
2026-09-23 08:34:08 +0300

    n

--- inform/swap.cpp
+++ inform/swap.cpp
@@ -4,11 +4,24 @@ using namespace std;
 
 int main()
 {    
-    int n;
-    cin >> n;
+    int h1, m1, s1, h2, m2, s2;
+    
+    cin >> h1;
+    cin >> m1;
+    cin >> s1;
+    cin >> h2;
+    cin >> m2;
+    cin >> s2;
 
-    int t = 0;
-    t += (45 * n) + ((n - 1)/2 * 15) + (n / 2) * 5;
+    int t1 = s1 + m1 * 60 + h1 * 3600;
+    int t2 = s2 + m2 * 60 + h2 * 3600;
 
-    cout << (t / 60) + 9 << " " << t % 60 << endl;
+    if (t1 > t2)
+    {    
+        cout << t1 - t2 << endl;
+    }
+    else 
+    {    
+        cout << t2 - t1 << endl;
+    }
 }
