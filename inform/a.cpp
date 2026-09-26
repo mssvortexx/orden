f2a940e10d5fafc22c769869b959344cfe51fdb8
Светка <mssvortex@icloud.com>
2026-09-26 11:42:30 +0300

    ,

---
+++ inform/a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{    
+    int x;
+    cin >> x;
+
+    if (x % 4 == 0 && (x % 400 == 0 || x % 100 != 0))
+    {    
+        cout << "YES" << endl;
+    }
+    else
+    {    
+        cout << "NO" << endl;
+    }
+
+}
